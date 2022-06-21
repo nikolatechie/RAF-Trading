ae0c9dac0b1c5cb4b63ec1b65d8f655fe0b3540b
Nikola Grujic <nikola-grujic@hotmail.com>
2022-06-21 18:48:30 +0200

    Initial commit

---
+++ app/src/main/java/rs/raf/jun/nikola_grujic_rn2419/MainActivity.kt
@@ -0,0 +1,11 @@
+package rs.raf.jun.nikola_grujic_rn2419
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
