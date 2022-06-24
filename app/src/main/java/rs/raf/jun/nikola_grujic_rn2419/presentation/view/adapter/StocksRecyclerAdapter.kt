7410ce8e6af5a2b6e2e84c5e0fd89d7a3455406e
Nikola Grujic <nikola-grujic@hotmail.com>
2022-06-24 18:47:17 +0200

    Added drawing colored charts

--- app/src/main/java/rs/raf/jun/nikola_grujic_rn2419/presentation/view/adapter/StocksRecyclerAdapter.kt
+++ app/src/main/java/rs/raf/jun/nikola_grujic_rn2419/presentation/view/adapter/StocksRecyclerAdapter.kt
@@ -1,11 +1,19 @@
 package rs.raf.jun.nikola_grujic_rn2419.presentation.view.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import rs.raf.jun.nikola_grujic_rn2419.R
+import rs.raf.jun.nikola_grujic_rn2419.data.model.Bar
 import rs.raf.jun.nikola_grujic_rn2419.data.model.Quote
 
 class StocksRecyclerAdapter(private var list: ArrayList<Quote>) :
@@ -15,6 +23,7 @@ class StocksRecyclerAdapter(private var list: ArrayList<Quote>) :
         var symbol: TextView = view.findViewById(R.id.symbolRight)
         var name: TextView = view.findViewById(R.id.stockName)
         var lastPrice: TextView = view.findViewById(R.id.lastPrice)
+        var chart: LineChart = view.findViewById(R.id.stockChart)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -28,7 +37,31 @@ class StocksRecyclerAdapter(private var list: ArrayList<Quote>) :
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.symbol.text = list[position].symbol
         holder.name.text = "Name: " + list[position].name
-        holder.lastPrice.text =  "Last price: " + list[position].last.toString() + list[position].currency
+        holder.lastPrice.text =
+            "Last price: " + list[position].last.toString() + list[position].currency
+
+        val entries = ArrayList<Entry>()
+        val colors = ArrayList<Int>()
+
+        for (bar: Bar in list[position].chart.bars)
+            entries.add(Entry(entries.size.toFloat(), (bar.price).toFloat()))
+
+        val set = LineDataSet(entries, "Stock")
+
+        if (list[position].changeFromPreviousClose < 0f) {
+            set.color = Color.RED
+            set.setCircleColor(Color.RED)
+        }
+        else {
+            set.color = Color.GREEN
+            set.setCircleColor(Color.GREEN)
+        }
+
+        val dataSet = ArrayList<ILineDataSet>()
+        dataSet.add(set)
+        val data = LineData(dataSet)
+        holder.chart.data = data
+        holder.chart.invalidate()
     }
 
     override fun getItemCount(): Int {
